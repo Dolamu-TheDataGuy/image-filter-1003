b7b6460a8b51003dc3013b0b811c4db9baaf7499
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-01-20 09:34:00 +0100

    added updated server code

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import {Router, Request, Response} from 'express'
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -29,11 +30,23 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+
   //! END @TODO1
+  app.get( "/filteredimage", async ( req: Request, res: Response ) => {
+    const image_url = req.query.image_url.toString();
+    if ( !image_url ) {
+      res.status(400).send("image_url is required");
+    }
+    const filteredpath : string = await filterImageFromURL(image_url);
+
+    res.status(200).sendFile(filteredpath, () => {
+      deleteLocalFiles([filteredpath]);
+    });
+  } );
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
